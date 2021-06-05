c667569449ca46dd4c2aa0fe8fbe3054231f1e3d
Sajith Lakjaya Siriwardhana <sajithlakjaya@outlook.com>
2021-06-05 19:09:11 +0530

    Add missing model

---
+++ src/app/shared/models/metadata.ts
@@ -0,0 +1,20 @@
+export interface IStoreType {
+	id: number;
+	name: string;
+}
+
+export interface IMassUnit {
+	id: number;
+	name: string;
+}
+
+export interface IDistanceUnit {
+	id: number;
+	name: string;
+}
+
+export interface IMetaData {
+	storeTypes: IStoreType[];
+	massUnits: IMassUnit[];
+	distanceUnits: IDistanceUnit[];
+}
